123cf99a23d6ea2eac88c99b9614d7b5df35dec9
rojae <rojaerichards@live.co.uk>
2024-12-27 20:24:21 +0000

    Take brand and model of a car or bike then print it.

--- main.py
+++ main.py
@@ -13,22 +13,45 @@ class vehicle:
 
 class car(vehicle):
     pass
+
+class bike(vehicle):
+    pass
 def makeCar():
 
 
-    start = input("build a new car? y/n")
+    start = input("build a new car? y/n ")
 
     if "y" in start:
-        car_brand = input("what brand is the car?")
-        car_model = input("what model is the car?")
-        c = car(car_brand,car_model)
+        car_brand = input("what brand is the car? ")
+        car_model = input("what model is the car? ")
+        c = car (car_brand,car_model)
         print(c)
-
-        return 
+        makeCar()
     else:
         print("come back when you want a car...ok bub?")
+        makeBike()
         exit()
         
 
+def makeBike():
 
-makeCar()
+    start = input("would you like to make a bike?")
+
+    if "y" in start:
+        bike_brand = input("what brand?: ")
+        bike_model = input("what model:  ")
+        b = bike (bike_brand,bike_model)
+        print(b)
+        makeBike()
+    else:
+        print("aye get outta here if you ain't talking bike")
+        return makeCar()
+        exit()
+
+
+begin = input("start? y/n")
+if "y" in begin:
+    makeCar()
+else:
+    print("scram")
+    exit()
